9077344ac564a2e6449a53a0d559797ef250fe33
TehamiKamdar <tehamikamdar15@gmail.com>
2026-01-28 15:05:59 +0500

    minor changes

--- app/Services/RecaptchaEnterpriseService.php
+++ app/Services/RecaptchaEnterpriseService.php
@@ -2,33 +2,37 @@
 
 namespace App\Services;
 
-use Google\Cloud\RecaptchaEnterprise\V1\Assessment;
-use Google\Cloud\RecaptchaEnterprise\V1\Client\RecaptchaEnterpriseServiceClient;
 use Google\Cloud\RecaptchaEnterprise\V1\Event;
+use Google\Cloud\RecaptchaEnterprise\V1\Assessment;
+use Google\Cloud\RecaptchaEnterprise\V1\CreateAssessmentRequest;
+use Google\Cloud\RecaptchaEnterprise\V1\Client\RecaptchaEnterpriseServiceClient;
 
 class RecaptchaEnterpriseService
 {
     public function verify(string $token, string $action, float $minScore = 0.5): bool
     {
-        // Absolute path to your JSON file
         $credentialsPath = base_path('storage/app/google/abiding-ripple-485604-u0-0ceac760a2d1.json');
 
-        // Pass credentials directly
         $client = new RecaptchaEnterpriseServiceClient([
             'credentials' => $credentialsPath,
         ]);
 
+        // Event
         $event = (new Event)
             ->setSiteKey(config('services.recaptcha.site_key'))
             ->setToken($token);
 
+        // Assessment
         $assessment = (new Assessment)
             ->setEvent($event);
 
-        $response = $client->createAssessment(
-            $client->projectName(config('services.recaptcha.project_id')),
-            $assessment
-        );
+        // Build request object
+        $request = (new CreateAssessmentRequest)
+            ->setParent($client->projectName(config('services.recaptcha.project_id')))
+            ->setAssessment($assessment);
+
+        // Make request
+        $response = $client->createAssessment($request);
 
         // Token invalid
         if (! $response->getTokenProperties()->getValid()) {

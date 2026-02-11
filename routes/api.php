b9878862a9086074d234020c3348f81b49b254ea
TehamiKamdar <tehamikamdar15@gmail.com>
2026-02-11 16:03:07 +0500

    documents delete testing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DocumentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// routes/api.php
+Route::delete('/delete/student/document/{id}', [DocumentController::class, 'destroy'])
+    ->middleware('auth:sanctum'); // ya apni API auth
+
--- routes/web.php
+++ routes/web.php
@@ -65,4 +65,3 @@ Route::post('student/register', [RegisterController::class , 'store'])->name('st
 Route::get('generate/student/profile/{id}', [PdfController::class , 'downloadPdf']);
 Route::get('view/student/profile/{id}', [PdfController::class , 'viewPdf']);
 Route::get('download/student/documents/{folder}', [DocumentController::class , 'downloadDocuments']);
-Route::delete('delete/student/documents/{$studentId}', [DocumentController::class, 'destroy'])->name('student.document.destroy');

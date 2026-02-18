1d645b3b6f444878294fa970a91fb6e45a47ebde
TehamiKamdar <tehamikamdar15@gmail.com>
2026-02-18 15:26:56 +0500

    minor changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -184,7 +184,7 @@ class HomeController extends Controller
 
         // 6️⃣ Prepare data for DB + Mail
         $data = [
-            'ip' => $ip,
+            'ip' => $$validated['ip'],
             'name' => strtolower($validated['name']),
             'email' => strtolower($validated['email']),
             'message' => strtolower($validated['message']) ?? '',

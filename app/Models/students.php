4a10cbbf266e4a613dd0eb6688e8fc8477f61c3b
TehamiKamdar <tehamikamdar15@gmail.com>
2026-09-11 11:37:03 +0500

    multiple countries option added

--- app/Models/students.php
+++ app/Models/students.php
@@ -13,11 +13,13 @@ class students extends Model
 
     protected $fillable = ['first_name','last_name','father_name','mother_name','dob','email','phone','city','address','postal_code','cnic','english_test','english_proficiency','passport_number','passport_valid_from','passport_valid_thru','account_created','status','country_id','program_level_id','intake','qualification','percentage','secondary_email','secondary_password'];
 
-    
+    protected $casts = [
+        'country_id' => 'array',
+    ];
 
     public function getCountryNamesAttribute()
     {
-        $countryIds = json_decode($this->country_id, true) ?? [];
+        $countryIds = $this->country_id ?? [];
 
         return \App\Models\Country::whereIn('id', $countryIds)
             ->pluck('name')
